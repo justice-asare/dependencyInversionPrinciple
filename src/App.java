2563f8458ea9f08c0c1afd627a9c252b4d824f62
Justice Owusu Asare <justice-asare.owusu@turntabl.io>
2022-01-12 14:03:17 +0000

    main class functionality added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
 
+        DatabaseHandler databaseHandler = new DatabaseHandler(new MySQLDatabase());
+        databaseHandler.connect();
+        databaseHandler.disconnect();
+
     }
 }
